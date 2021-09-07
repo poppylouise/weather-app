31182278129eb74cbd3b416a15bf80d8af5153c7
poppylouise <86498404+poppylouise@users.noreply.github.com>
2021-09-07 15:34:02 +0100

    restructure

--- weather.js
+++ weather.js
@@ -1,10 +1,10 @@
 // 7 day forecast
 
 function formatDay(timestamp) {
-let date = new Date(timestamp * 1000);
-let day = date.getDay();
-let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-return days[day];
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
 }
 
 function displayForecast(response) {
@@ -16,21 +16,30 @@ function displayForecast(response) {
 
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
-    forecastHTML =
-      forecastHTML +
-      `
+      forecastHTML =
+        forecastHTML +
+        `
                 <div class="col-2">
-                  <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+                  <div class="weather-forecast-date">${formatDay(
+                    forecastDay.dt
+                  )}</div>
                   <img
-                    src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+                    src="http://openweathermap.org/img/wn/${
+                      forecastDay.weather[0].icon
+                    }@2x.png"
                     alt=""
                     width="42"/>
                   <div class="weather-forecast-temperatures">
-                    <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}° </span>
-                    <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}° </span>
+                    <span class="weather-forecast-temperature-max">${Math.round(
+                      forecastDay.temp.max
+                    )}° </span>
+                    <span class="weather-forecast-temperature-min">${Math.round(
+                      forecastDay.temp.min
+                    )}° </span>
                   </div>
                 </div>`;
-  }});
+    }
+  });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -131,6 +140,7 @@ function handleSubmit(event) {
     alert("Please enter a city");
   }
 }
+
 let city = document.querySelector("#search-city-input");
 
 let formSearch = document.querySelector("form");
